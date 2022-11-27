3cd9625248e30b8041d4451a88240d3f4bf2baf9
cgalang <cgalang11@hotmail.com>
2022-11-27 15:33:46 -0800

    Add css and dropdown menu to nav

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,36 +1,52 @@
-
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import { NavLink, useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import './NavBar.css'
+import { logout } from '../store/session';
 
 const NavBar = () => {
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const user = useSelector((state) => state.session)
+
+  const onLogout = async (e) => {
+    await dispatch(logout());
+    history.push('/')
+  }
+
+  const toggleMenu = () => {
+    const menu = document.querySelector('#dropdown_menu')
+    if(menu.classList.contains('hidden')) {
+      menu.classList.remove('hidden')
+    } else {
+      menu.classList.add('hidden')
+    }
+  }
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+    <nav id='nav'>
+      <div id='logo'>
+        LOGO HERE
+      </div>
+      <div id='menu' onClick={toggleMenu}>
+        <img src='https://s3.amazonaws.com/splitwise/uploads/user/default_avatars/avatar-teal19-200px.png' alt='user_icon' id='nav_user_icon' />
+        {user.user && <div>{user.user.username}</div>}
+        <i className="fa-solid fa-caret-down" />
+      </div>
+      <div id='dropdown_menu' className='hidden'>
+        <div className='dropdown_menu_item' onClick={() => history.push('/dashboard')}>
+          Dashboard
+        </div>
+        <div className='dropdown_menu_item' onClick={() => history.push('/create-group')}>
+          Create a group
+        </div>
+        <div className='dropdown_menu_item' onClick={() => history.push('/add-friend')}>
+          Add a friend
+        </div>
+        <div className='dropdown_menu_item' onClick={onLogout}>
+          Log out
+        </div>
+      </div>
     </nav>
   );
 }
