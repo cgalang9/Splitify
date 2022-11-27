ba2a044d7045f71ed2c411dca9b874cdae2bd046
cgalang <cgalang11@hotmail.com>
2022-11-26 19:19:36 -0800

    Create all expenses page

--- react-app/src/components/Main/index.js
+++ react-app/src/components/Main/index.js
@@ -12,6 +12,7 @@ import AddPaymentForm from '../AddPaymentForm';
 import EditPaymentForm from '../EditPaymentForm';
 import AddFriendForm from '../AddFriendForm';
 import CreateGroup from '../CreateGroup';
+import AllExpenses from '../AllExpenses';
 
 function Main() {
     const history = useHistory()
@@ -33,6 +34,7 @@ function Main() {
                             <Route exact path="/payments/:paymentId/edit-payment" component={() => <EditPaymentForm />} />
                             <Route exact path="/add-friend" component={() => <AddFriendForm />} />
                             <Route exact path="/create-group" component={() => <CreateGroup />} />
+                            <Route exact path="/all" component={() => <AllExpenses />} />
                             <Route exact path="/error">
                                 <h1>There was an error</h1>
                             </Route>
