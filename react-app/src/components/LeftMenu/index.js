6f3abcb3cc1e009605f2508c9e5f5b48f4541675
cgalang <cgalang11@hotmail.com>
2022-11-28 15:46:54 -0800

    Added css to left menu component

--- react-app/src/components/LeftMenu/index.js
+++ react-app/src/components/LeftMenu/index.js
@@ -20,21 +20,39 @@ const LeftMenu = () => {
 
     return (
         <div id='left_menu_wrapper' className='flex_col'>
-            <NavLink to='/dashboard' style={{ textDecoration: 'none' }}><div id='dashboard_link'>Dashboard</div></NavLink>
-            <NavLink to='/all' style={{ textDecoration: 'none' }}><div>All Expenses</div></NavLink>
-            <div id='left_menu_groups'>
-                <div id='left_menu_groups_head'>GROUPS <button onClick={() => history.push('/create-group')}><i className="fa-solid fa-plus" />add</button></div>
+            <NavLink to='/dashboard'
+                className='left_nav_link'
+                style={{ textDecoration: 'none', color: 'grey', padding: '0 10px'}}
+                activeStyle={{ color: "#70caae", borderLeft: '3px solid #70caae'}}
+            >
+                <i className="fa-solid fa-envelope" />Dashboard
+            </NavLink>
+            <NavLink to='/all'
+                className='left_nav_link'
+                style={{ textDecoration: 'none', color: 'grey', padding: '0 10px'}}
+                activeStyle={{ color: "#70caae", borderLeft: '3px solid #70caae'}}
+            >
+                <i className="fa-solid fa-bars" />All expenses
+            </NavLink>
+            <div id='left_menu_groups' className='flex_col'>
+                <div id='left_menu_groups_head'>GROUPS <button onClick={() => history.push('/create-group')} className='add_btn'><i className="fa-solid fa-plus" />add</button></div>
                 {user_groups && user_groups.groups.map(group => (
                     <div key={group.id} className='left_menu_group_li'>
-                        <NavLink to={`/groups/${group.id}`} style={{ textDecoration: 'none' }}><div>{group.name}</div></NavLink>
+                        <NavLink to={`/groups/${group.id}`}
+                            className='left_nav_link'
+                            style={{ textDecoration: 'none', color: 'grey', padding: '0 10px'}}
+                            activeStyle={{ color: "#70caae", borderLeft: '3px solid #70caae'}}
+                        >
+                            <i className="fa-solid fa-tag" />{group.name}
+                        </NavLink>
                     </div>
                 ))}
             </div>
-            <div id='left_menu_friends'>
-                <div id='left_menu_friends_head'>FRIENDS <button onClick={() => history.push('/add-friend')}><i className="fa-solid fa-plus" />add</button></div>
+            <div id='left_menu_friends' className='flex_col'>
+                <div id='left_menu_friends_head'>FRIENDS <button className='add_btn' onClick={() => history.push('/add-friend')}><i className="fa-solid fa-plus" />add</button></div>
                 {friends && friends.currUserFriends.map(friend => (
                     <div key={friend.id} className='left_menu_friend_li'>
-                        <div>{friend.username}</div>
+                        <i className="fa-solid fa-user" />{friend.username}
                     </div>
                 ))}
             </div>
