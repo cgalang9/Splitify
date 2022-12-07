67492c71b8eb71eef970e96023acc9bc4d9bf96c
cgalang <cgalang11@hotmail.com>
2022-12-07 12:47:42 -0800

    Added backend route to delete freindship

--- app/api/friends_routes.py
+++ app/api/friends_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.models import Group, Expense, UsersExpenses, ExpenseComment, Payment, PaymentComment, UsersGroups, User, Friendship, db
 from sqlalchemy.orm import joinedload
-from sqlalchemy import or_
+from sqlalchemy import or_, and_
 
 friends_routes = Blueprint('friends', __name__)
 
@@ -41,7 +41,7 @@ def get_current_user_friends():
 @login_required
 def create_friendship():
     """
-    Create friendship (wuth current user)
+    Create friendship (with current user)
     """
     req = request.json
     friend_id = int(req.get('friend_id'))
@@ -79,3 +79,43 @@ def create_friendship():
     db.session.commit()
 
     return { "id": friend.id, "username": friend.username }
+
+
+@friends_routes.delete('')
+@login_required
+def delete_friendship():
+    """
+    Delete friendship (with current user)
+    """
+    req = request.json
+    friend_id = int(req.get('friend_id'))
+    curr_user_id = int(current_user.get_id())
+
+    # validation: can not friend yourself
+    if friend_id == curr_user_id:
+        return {"error": "Can not add or delete yourself as a friend"}, 400
+
+    friend = User.query.get(friend_id)
+
+    # validation: friend_id not found
+    if friend == None:
+        return {"error": "Friend not found"}, 404
+
+    # find friendship
+    freindshipA = Friendship.query.filter(and_(Friendship.user1_id == curr_user_id, Friendship.user2_id == friend_id)).all()
+    freindshipB = Friendship.query.filter(and_(Friendship.user2_id == curr_user_id, Friendship.user1_id == friend_id)).all()
+
+    print('=========================', freindshipB)
+
+    if len(freindshipA) == 0 and len(freindshipB) == 0 :
+        return {"error": "You are not friends with this user"}, 400
+
+    for friendship in freindshipA:
+        db.session.delete(friendship)
+        db.session.commit()
+
+    for friendship in freindshipB:
+        db.session.delete(friendship)
+        db.session.commit()
+
+    return {"message": "Expense Successfully deleted"}
