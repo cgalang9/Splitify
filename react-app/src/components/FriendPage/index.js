f87d4167d122de07688099255841b968220c7914
cgalang <cgalang11@hotmail.com>
2022-12-27 08:38:26 -0800

    Add css and redict if friend is not curr user friend to friend page

--- react-app/src/components/FriendPage/index.js
+++ react-app/src/components/FriendPage/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { getCurrUserExpensesThunk } from "../../store/expenses";
 import { getCurrUserPaymentsThunk } from "../../store/payments";
 import "./FriendPage.css";
@@ -9,8 +9,35 @@ import AddExpenseFormModal from "../AddExpenseForm";
 import AddPaymentFormModal from "../AddPaymentForm";
 import github from "../../assests/github.png";
 import user_icon_img from "../../assests/user_icon_img.png";
+import { deleteFriendThunk } from "../../store/currUserFriends";
 
 export default function FriendPage() {
+  const { friendId } = useParams();
+  const history = useHistory();
+  const friends = useSelector((state) => state.currUserFriends);
+  const user = useSelector((state) => state.session);
+
+  const [currFriend, setCurrFriend] = useState(null);
+
+  //redirect if not logged in or not part of group or group not found
+  useEffect(() => {
+    if (!user.user) {
+      history.push("/");
+    }
+  }, []);
+
+  useEffect(() => {
+    let friend = null;
+    if (friends) {
+      friend = friends.currUserFriends.find((fr) => fr.id === Number(friendId));
+      if (!friend) {
+        history.push("/dashboard"); //redirect to dash if friend is not actually friends with curr user
+      } else {
+        setCurrFriend(friend);
+      }
+    }
+  }, [friendId, friends]);
+
   return (
     <div id="friends_page_wrapper">
       <div id="friends_page_left">
@@ -18,25 +45,35 @@ export default function FriendPage() {
       </div>
       <div id="friends_page_mid" className="flex_col">
         <div id="friends_page_head">
-          <div id="friends_page_top">
-            <div id="friends_page_title">Friend Name Here</div>
-            <div id="friends_page_buttons_container">
+          <div id="friends_page_head_top">
+            <div id="friends_page_head_title">
+              <img src={user_icon_img} alt="user_icon" />
+              <div>{currFriend ? currFriend.username : ""}</div>
+            </div>
+            <div id="friends_page_head_buttons_container">
               <AddExpenseFormModal />
               <AddPaymentFormModal />
             </div>
           </div>
         </div>
-        <div id="dash_mid_main"></div>
+        <div id="friends_page_head_bottom"></div>
       </div>
-      <div id="dash_right">
-        <div id="dash_right_title">MY LINKS</div>
-        <div id="dash_right_github_container">
+      <div id="friends_page_right">
+        {/* <div id="friends_page_delete_friend">
+          <button>Delete Friend</button>
+        </div> */}
+        <div id="friends_page_right_title">MY LINKS</div>
+        <div id="friends_page_right_github_container">
           <a
             target="_blank"
             rel="noopener noreferrer"
             href="https://github.com/cgalang9"
           >
-            <img src={github} alt="github_logo" id="dash_right_github" />
+            <img
+              src={github}
+              alt="github_logo"
+              id="friends_page_right_github"
+            />
           </a>
         </div>
         <div id="linkedin_container">
